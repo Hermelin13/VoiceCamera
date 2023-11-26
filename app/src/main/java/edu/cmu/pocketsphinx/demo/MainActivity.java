a01fbd6feab94982882f875f398dd3200c6c4a7a
Hermelín <adamdaliborj@gmail.com>
2023-11-26 18:01:51 +0100

    spousti se

---
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/MainActivity.java
@@ -0,0 +1,391 @@
+package edu.cmu.pocketsphinx.demo;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.*;
+import androidx.annotation.NonNull;
+import androidx.camera.core.AspectRatio;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.video.MediaStoreOutputOptions;
+import androidx.camera.video.Quality;
+import androidx.camera.video.QualitySelector;
+import androidx.camera.video.Recorder;
+import androidx.camera.video.Recording;
+import androidx.camera.video.VideoCapture;
+import androidx.camera.video.VideoRecordEvent;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+import edu.cmu.pocketsphinx.Assets;
+import edu.cmu.pocketsphinx.Hypothesis;
+import edu.cmu.pocketsphinx.RecognitionListener;
+import edu.cmu.pocketsphinx.SpeechRecognizer;
+import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
+
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import static android.widget.Toast.makeText;
+
+
+public class MainActivity extends AppCompatActivity implements RecognitionListener {
+
+    Recording recording = null;
+    VideoCapture<Recorder> videoCapture = null;
+    ImageButton capture, toggleFlash, flipCamera;
+    PreviewView previewView;
+    int cameraFacing = CameraSelector.LENS_FACING_BACK;
+    private ImageCapture imageCapture;
+
+    private final ActivityResultLauncher<String> cameraPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), result -> {
+        if (result) {
+            startCamera(cameraFacing);
+        }
+    });
+
+
+    private static final String MAIN = "wakeup";
+    private static final String KEYPHRASE = "action";
+    private static final String KEYPHOTO = "digits";
+    private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+    private SpeechRecognizer recognizer;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+
+        setContentView(R.layout.main);
+        previewView = findViewById(R.id.cameraPreview);
+        capture = findViewById(R.id.capture);
+        toggleFlash = findViewById(R.id.toggleFlash);
+        flipCamera = findViewById(R.id.flipCamera);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            cameraPermissionLauncher.launch(Manifest.permission.CAMERA);
+        } else {
+            startCamera(cameraFacing);
+        }
+
+        flipCamera.setOnClickListener(view -> {
+            if (cameraFacing == CameraSelector.LENS_FACING_BACK) {
+                cameraFacing = CameraSelector.LENS_FACING_FRONT;
+            } else {
+                cameraFacing = CameraSelector.LENS_FACING_BACK;
+            }
+            startCamera(cameraFacing);
+        });
+
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
+            return;
+        }
+        // Recognizer initialization is a time-consuming and it involves IO,
+        // so we execute it in async task
+        new SetupTask(this).execute();
+    }
+
+    private static class SetupTask extends AsyncTask<Void, Void, Exception> {
+        WeakReference<MainActivity> activityReference;
+
+        SetupTask(MainActivity activity) {
+            this.activityReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected Exception doInBackground(Void... params) {
+            try {
+                Assets assets = new Assets(activityReference.get());
+                File assetDir = assets.syncAssets();
+                activityReference.get().setupRecognizer(assetDir);
+            } catch (IOException e) {
+                return e;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Exception result) {
+            if (result != null) {
+                Log.d("SpeechRecognition", "Failed to init recognizer " + result);
+            } else {
+                activityReference.get().startSpotting();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Recognizer initialization is a time-consuming and it involves IO,
+                // so we execute it in async task
+                new SetupTask(this).execute();
+            } else {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (recognizer != null) {
+            recognizer.cancel();
+            recognizer.shutdown();
+        }
+    }
+
+    /**
+     * In partial result we get quick updates about current hypothesis.
+     * In keyword spotting mode, react when the keyword is spotted.
+     */
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onPartialResult(Hypothesis hypothesis) {
+        if (hypothesis == null)
+            return;
+
+        String text = hypothesis.getHypstr();
+        Log.d("SpeechRecognition", "Partial Result: " + text);
+
+        if (text.equals(KEYPHRASE)) {
+            makeText(getApplicationContext(), "Keyword Spotted: " + text, Toast.LENGTH_SHORT).show();
+        }
+        else if (text.equals(KEYPHOTO)) {
+            makeText(getApplicationContext(), "Keyword Spotted: " + text, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * This callback is called when we stop the recognizer.
+     */
+    @Override
+    public void onResult(Hypothesis hypothesis) {
+        if (hypothesis != null) {
+
+            String text = hypothesis.getHypstr();
+            /*if (text.equals(KEYPHRASE)){
+                makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            }
+            if (text.equals(KEYPHOTO)){
+                makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            }*/
+        }
+        recognizer.startListening(MAIN);
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+    }
+
+    /**
+     * We stop recognizer here to get a final result
+     */
+    @Override
+    public void onEndOfSpeech() {
+        recognizer.stop();
+    }
+
+    private void startSpotting() {
+        recognizer.startListening(MAIN); // Start listening for the keyword
+    }
+
+    private void setupRecognizer(File assetsDir) throws IOException {
+        recognizer = SpeechRecognizerSetup.defaultSetup()
+                .setAcousticModel(new File(assetsDir, "en-us-ptm"))
+                .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
+                .getRecognizer();
+        recognizer.addListener(this);
+
+        File menuGrammar = new File(assetsDir, "menu.gram");
+        recognizer.addKeywordSearch(MAIN, menuGrammar);
+    }
+
+    @Override
+    public void onError(Exception error) {
+        Log.d("SpeechRecognition", Objects.requireNonNull(error.getMessage()));
+    }
+
+    @Override
+    public void onTimeout() {
+        // This method is not used for continuous keyword spotting.
+    }
+
+    public void startCamera(int cameraFacing) {
+        int aspectRatio = aspectRatio(previewView.getWidth(), previewView.getHeight());
+        ListenableFuture<ProcessCameraProvider> listenableFuture = ProcessCameraProvider.getInstance(this);
+
+        listenableFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = listenableFuture.get();
+
+                Preview preview = new Preview.Builder().setTargetAspectRatio(aspectRatio).build();
+
+                imageCapture = new ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                        .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
+
+                Recorder recorder = new Recorder.Builder()
+                        .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
+                        .build();
+                videoCapture = VideoCapture.withOutput(recorder);
+
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(cameraFacing).build();
+
+                cameraProvider.unbindAll();
+
+                Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, videoCapture, imageCapture);
+
+                capture.setOnClickListener(view -> {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        cameraPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                    } else {
+                        captureVideo();
+                    }
+                });
+
+                toggleFlash.setOnClickListener(view -> setFlashIcon(camera));
+
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    public void captureVideo() {
+        Recording recording1 = recording;
+        if (recording1 != null) {
+            recording1.stop();
+            recording = null;
+            return;
+        }
+        String name = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.getDefault()).format(System.currentTimeMillis());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4");
+        contentValues.put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video");
+
+        MediaStoreOutputOptions options = new MediaStoreOutputOptions.Builder(getContentResolver(), MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+                .build();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        recording = videoCapture.getOutput().prepareRecording(MainActivity.this, options).withAudioEnabled().start(ContextCompat.getMainExecutor(MainActivity.this), videoRecordEvent -> {
+            if (videoRecordEvent instanceof VideoRecordEvent.Start) {
+                capture.setEnabled(true);
+
+                // Set the duration for video capture (5 seconds in this example)
+                long captureDurationMillis = 5000;
+
+                // Schedule a task to stop the recording after the specified duration
+                new Handler().postDelayed(() -> {
+                    if (recording != null) {
+                        recording.stop();
+                        recording = null;
+                    }
+                }, captureDurationMillis);
+
+            } else if (videoRecordEvent instanceof VideoRecordEvent.Finalize) {
+                if (!((VideoRecordEvent.Finalize) videoRecordEvent).hasError()) {
+                    String msg = "Video capture succeeded: " + ((VideoRecordEvent.Finalize) videoRecordEvent).getOutputResults().getOutputUri();
+                    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+                } else {
+                    recording.close();
+                    recording = null;
+                    String msg = "Error: " + ((VideoRecordEvent.Finalize) videoRecordEvent).getError();
+                    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void takePicture() {
+        final File picturesDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        if (!picturesDirectory.exists()) {
+            picturesDirectory.mkdirs();
+        }
+
+        String imageFileName = "IMG_" + System.currentTimeMillis() + ".jpg";
+        final File file = new File(picturesDirectory, imageFileName);
+
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(file).build();
+        imageCapture.takePicture(outputFileOptions, Executors.newCachedThreadPool(), new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                // Add image to the MediaStore
+                ContentValues contentValues = new ContentValues();
+                contentValues.put(MediaStore.Images.Media.DATA, file.getAbsolutePath());
+                contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+                getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+
+                runOnUiThread(() -> Toast.makeText(MainActivity.this, "Image saved at: " + file.getPath(), Toast.LENGTH_SHORT).show());
+                startCamera(cameraFacing);
+            }
+
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+                runOnUiThread(() -> Toast.makeText(MainActivity.this, "Failed to save: " + exception.getMessage(), Toast.LENGTH_SHORT).show());
+                startCamera(cameraFacing);
+            }
+        });
+    }
+
+    private void setFlashIcon(Camera camera) {
+        if (camera.getCameraInfo().hasFlashUnit()) {
+            if (camera.getCameraInfo().getTorchState().getValue() == 0) {
+                camera.getCameraControl().enableTorch(true);
+                toggleFlash.setImageResource(R.drawable.baseline_flash_off_24);
+            } else {
+                camera.getCameraControl().enableTorch(false);
+                toggleFlash.setImageResource(R.drawable.baseline_flash_on_24);
+            }
+        } else {
+            runOnUiThread(() -> Toast.makeText(MainActivity.this, "Flash is not available currently", Toast.LENGTH_SHORT).show());
+        }
+    }
+
+    private int aspectRatio(int width, int height) {
+        double previewRatio = (double) Math.max(width, height) / Math.min(width, height);
+        if (Math.abs(previewRatio - 4.0 / 3.0) <= Math.abs(previewRatio - 16.0 / 9.0)) {
+            return AspectRatio.RATIO_4_3;
+        }
+        return AspectRatio.RATIO_16_9;
+    }
+}
